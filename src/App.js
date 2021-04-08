88289231630a00d739d4941933a29f265adcbf5c
samrock5000 <35318078+samrock5000@users.noreply.github.com>
2021-04-08 10:47:49 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ControlledInputs from './form-list'
+
+function App() {
+  return (
+    <div className='container'>
+      <ControlledInputs/>
+     
+    </div>
+  )
+}
+
+export default App
